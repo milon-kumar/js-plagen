548dbecbbc481ae328122689ed8bf5da7d3ce848
Jugol Kumar <jk23717933@gmail.com>
2022-03-25 20:01:19 +0600

    Typed js plagen add in a project header section

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#type', {
+    // Waits 1000ms after typing "First"
+    strings: ['Ux/Ui Designer', 'Web Developer.','API Developer.','Graphics Designer'],
+    loop:true,
+    typeSpeed:150,
+    startDelay:5,
+    backSpeed:80,
+});
